099d657e488f097a61a272e56b032371977d7fe9
biWenZhe <2368688782@qq.com>
2022-11-18 15:23:18 +0800

    mian

--- answer/src/main.js
+++ answer/src/main.js
@@ -1,9 +1,12 @@
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk'
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false;
 
 new Vue({
