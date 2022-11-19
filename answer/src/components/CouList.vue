57975d76ab44455c61d495abbbb02121e5f333da
biWenZhe <2368688782@qq.com>
2022-11-19 17:29:09 +0800

    main

--- answer/src/components/CouList.vue
+++ answer/src/components/CouList.vue
@@ -1,21 +1,41 @@
 <template>
         <el-table
+        height="250"
+        border
         :stripe="tableConfig.isStripe"
         :data="tableData"
-        style="width: 100%">
+        style="width: 100%; min-height: 400px; margin-top: 10px;">
             <el-table-column
+                sortable
                 prop="date"
-                label="日期"
+                label="创建日期"
                 width="180">
+            <template slot-scope="scope">
+                <i class="el-icon-time"></i>
+                <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            </template>
             </el-table-column>
             <el-table-column
+                sortable
                 prop="name"
-                label="姓名"
+                label="课程"
                 width="180">
             </el-table-column>
             <el-table-column
+                sortable
                 prop="address"
-                label="地址">
+                label="班级">
+            </el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button
+                    size="mini"
+                    @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button
+                    size="mini"
+                    type="danger"
+                    @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
             </el-table-column>
         </el-table>
 </template>
@@ -48,6 +68,14 @@ export default {
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1516 弄'
             }]
+        }
+    },
+    methods:{
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+        handleDelete(index, row) {
+            console.log(index, row);
         }
     }
 }
